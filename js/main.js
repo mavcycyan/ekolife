75ae914b699635caa5ac8427891528034786b8ff
mavcycyan <pbl6ewka@gmail.com>
2019-03-28 16:51:29 +0200

    - сделана шапка, первый и половина второго экрана страницы Главная

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/**/
+jQuery(document).ready(function($){	
+    $(".ath").fancybox({ 
+        href: '#modal_auth', 
+        wrapCSS: 'modal_auth_wp',
+        minWidth: '100%',
+        margin: [0, 0, 0, 0],
+        padding: [0, 0, 0, 0],
+        autoScale: true
+    });
+    $(".fp").fancybox({ 
+        href: '#modal_fp', 
+        wrapCSS: 'modal_auth_wp',
+        minWidth: '100%',
+        margin: [0, 0, 0, 0],
+        padding: [0, 0, 0, 0],
+        autoScale: true
+    });
+    $(".mn").fancybox({ 
+        href: '#modal_mn', 
+        wrapCSS: 'modal_auth_wp',
+        minWidth: '100%',
+        margin: [0, 0, 0, 0],
+        padding: [0, 0, 0, 0],
+        autoScale: true
+    });
+});
+/**/
+
+/**/
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(1000).fadeOut('slow');
+    $('#svg_pr rect').animate({ width: 526 }, 1000, function() { });
+});
+/**/
+
+/**/
+/**/
+
+/**/
+/**/
+
+/**/
+/**/
